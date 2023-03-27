ee057d67a6655c313b7924b27479b5b7555ee7c7
nazmulhasan7447 <nazmulhasan747406@gmail.com>
2023-03-27 23:53:58 +0600

    added reset, decrement, and increment by amount feature

--- src/features/Counter.js
+++ src/features/Counter.js
@@ -1,15 +1,25 @@
 import React, { useState } from "react";
 import { useDispatch, useSelector } from "react-redux";
-import { increment } from "./slices/counterSlice";
+import {
+  increment,
+  decrement,
+  incrementByAmount,
+  reset,
+} from "./slices/counterSlice";
 
 const Counter = () => {
+  const [amount, setAmount] = useState(0);
   const counter = useSelector((state) => {
     return state.counter.count;
   });
 
   const dispatch = useDispatch();
 
-  console.log(counter);
+  const addAmount = Number(amount) || 0;
+
+  const amountChangeHandler = (value) => {
+    setAmount(value);
+  };
 
   return (
     <>
@@ -18,13 +28,42 @@ const Counter = () => {
           <div className="col-2"></div>
           <div className="col-8 p-15 tex-center d-flex justify-content-center mt-5">
             <div className="counter-body">
-              <h3>{counter}</h3>
+              <h3 className="text-center">{counter}</h3>
               <button
                 className="btn btn-primary"
                 onClick={() => dispatch(increment())}
               >
                 + Increment
               </button>
+
+              <button
+                className="btn btn-primary m-1"
+                onClick={() => dispatch(decrement())}
+              >
+                - Decrement
+              </button>
+
+              <div className="mt-4">
+                <input
+                  type="number"
+                  className="form-control"
+                  placeholder="Type amount"
+                  onChange={(e) => amountChangeHandler(e.target.value)}
+                />
+                <button
+                  className="btn btn-primary m-1"
+                  onClick={() => dispatch(incrementByAmount(addAmount))}
+                >
+                  + Add amount
+                </button>
+
+                <button
+                  className="btn btn-primary m-1"
+                  onClick={() => dispatch(reset())}
+                >
+                  Reset
+                </button>
+              </div>
             </div>
           </div>
           <div className="col-2"></div>
--- src/features/slices/counterSlice.js
+++ src/features/slices/counterSlice.js
@@ -11,9 +11,19 @@ export const counterSlice = createSlice({
     increment: (state) => {
       state.count += 1;
     },
+    decrement: (state) => {
+      state.count -= 1;
+    },
+    incrementByAmount: (state, action) => {
+      state.count += action.payload;
+    },
+    reset: (state) => {
+      state.count = 0;
+    },
   },
 });
 
-export const { increment } = counterSlice.actions;
+export const { increment, decrement, incrementByAmount, reset } =
+  counterSlice.actions;
 
 export default counterSlice.reducer;
